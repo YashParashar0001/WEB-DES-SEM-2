32bfb6c4547440c32c52d217e924b5351d3c4d1e
YashParashar0001 <parasharyash014@gmail.com>
2026-04-07 23:43:31 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Custom JS (currently empty)
-
-// Example: Navbar click log
-document.querySelectorAll('.nav-link').forEach(link => {
-  link.addEventListener('click', () => {
-    console.log('Navigation clicked:', link.textContent);
-  });
-});
